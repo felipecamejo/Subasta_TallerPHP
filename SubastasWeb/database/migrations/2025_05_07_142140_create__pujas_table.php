6702face67744c900bbf9ebdafdd439229f71ffa
Alcedepradera <pablo.deleon@estudiantes.utec.edu.uy>
2025-05-08 11:29:31 -0300

    Update 2025_05_07_142140_create__pujas_table.php
    algunos cambios en pujas

--- SubastasWeb/database/migrations/2025_05_07_142140_create__pujas_table.php
+++ SubastasWeb/database/migrations/2025_05_07_142140_create__pujas_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('_pujas', function (Blueprint $table) {
+        Schema::create('pujas', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
         });
     }
 
