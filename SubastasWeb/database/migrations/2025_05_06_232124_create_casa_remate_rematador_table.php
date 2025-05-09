87ccc61b6055a57405c553ba852268c0a3456a61
Felipe <23felipecamejo@gmail.com>
2025-05-09 16:47:59 -0300

    saque la id a la tabla intermedia casaRemate_rematador

--- SubastasWeb/database/migrations/2025_05_06_232124_create_casa_remate_rematador_table.php
+++ SubastasWeb/database/migrations/2025_05_06_232124_create_casa_remate_rematador_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('casa_remate_rematador', function (Blueprint $table) {
-            $table->id();
 
             $table->foreignId('casa_remate_id')->references('id')->on('casa_remates')->onDelete('cascade');
             $table->foreignId('rematador_id')->references('id')->on('rematadores')->onDelete('cascade');
