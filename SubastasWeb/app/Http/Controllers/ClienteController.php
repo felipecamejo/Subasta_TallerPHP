1a8543112ad0aee7e3d011559faac8e70ac16a46
renzoPeraza <renzo.peraza@estudiantes.utec.edu.uy>
2025-05-15 17:08:09 -0300

    Controller Cliente y Proveedor

---
+++ SubastasWeb/app/Http/Controllers/ClienteController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+         $clientes = Cliente::with('direccion')->get();
+        return response()->json($clientes, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'nombre' => 'required|string|max:255',
+            'cedula' => 'required|string|unique:usuarios,cedula',
+            'email' => 'required|email|unique:usuarios,email',
+            'telefono' => 'nullable|string',
+            'imagen' => 'nullable|string',
+            'calificacion' => 'nullable|numeric',
+            'notificaciones' => 'nullable|array', // el array de notificaciones lo habiamos eliminado no?
+        ]);
+
+        $cliente = Cliente::create($validated);
+        return response()->json($cliente, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+       $cliente = Cliente::with(['direccion', 'pujas'])->find($id);
+
+        if (!$cliente) {
+            return response()->json(['error' => 'Cliente no encontrado'], 404);
+        }
+
+        return response()->json($cliente, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+         $cliente = Cliente::find($id);
+
+        if (!$cliente) {
+            return response()->json(['error' => 'Cliente no encontrado'], 404);
+        }
+
+        $validated = $request->validate([
+            'nombre' => 'sometimes|string|max:255',
+            'cedula' => 'sometimes|string|unique:usuarios,cedula,' . $id,
+            'email' => 'sometimes|email|unique:usuarios,email,' . $id,
+            'telefono' => 'nullable|string',
+            'imagen' => 'nullable|string',
+            'calificacion' => 'nullable|numeric',
+            'notificaciones' => 'nullable|array', // el array de notificaciones lo habiamos eliminado no?
+        ]);
+
+        $cliente->update($validated);
+
+        return response()->json($cliente, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+         $cliente = Cliente::find($id);
+
+        if (!$cliente) {
+            return response()->json(['error' => 'Cliente no encontrado'], 404);
+        }
+
+        $cliente->delete();
+
+        return response()->json(['message' => 'Cliente eliminado con éxito'], 200);
+    }
+}
