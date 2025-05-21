6277a09b4b29eaf1742e6c6d2b4f52d7d6772e9f
Felipe <23felipecamejo@gmail.com>
2025-05-21 19:56:12 -0300

    avances

--- SubastasWeb/frontend/src/app/stream/stream.component.ts
+++ SubastasWeb/frontend/src/app/stream/stream.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-stream',
-  imports: [],
+  imports: [InputTextModule],
   templateUrl: './stream.component.html',
   styleUrl: './stream.component.scss'
 })
