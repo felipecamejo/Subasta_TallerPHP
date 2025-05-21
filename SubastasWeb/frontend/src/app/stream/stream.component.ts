4e96d499c19e4cf62958d36c2aa1cc6e35080712
Felipe <23felipecamejo@gmail.com>
2025-05-21 18:13:01 -0300

    creee un component y lo hice andar

---
+++ SubastasWeb/frontend/src/app/stream/stream.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stream',
+  imports: [],
+  templateUrl: './stream.component.html',
+  styleUrl: './stream.component.scss'
+})
+export class StreamComponent {
+
+}
