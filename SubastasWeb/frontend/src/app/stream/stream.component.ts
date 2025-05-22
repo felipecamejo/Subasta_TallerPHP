50f1b1e53b58fd58dde649730ae6bb5121087d72
Felipe <23felipecamejo@gmail.com>
2025-05-22 15:49:13 -0300

    ya casi terminado stream

--- SubastasWeb/frontend/src/app/stream/stream.component.ts
+++ SubastasWeb/frontend/src/app/stream/stream.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-stream',
   standalone: true,
-  imports: [InputTextModule, FormsModule],
+  imports: [InputTextModule, FormsModule, ButtonModule],
   templateUrl: './stream.component.html',
   styleUrls: ['./stream.component.scss']
 })
 export class StreamComponent {
+  timer: string = '2:47';
   value: string = '';
 }
