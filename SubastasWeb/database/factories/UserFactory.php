be51cd1c140e8c97b807cddf29131c2e3f864c51
Felipe <23felipecamejo@gmail.com>
2025-05-24 16:01:08 -0300

    arregle cosas extra

--- SubastasWeb/database/factories/UserFactory.php
+++ SubastasWeb/database/factories/UserFactory.php
@@ -5,9 +5,10 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\User;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+/*
+ //@extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
