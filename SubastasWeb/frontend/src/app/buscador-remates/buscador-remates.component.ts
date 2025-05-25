6d734150f6662108a7d6ee075aaaa4b8a823b6bc
Felipe <23felipecamejo@gmail.com>
2025-05-25 17:45:51 -0300

    bastante avanzada la pagina

--- SubastasWeb/frontend/src/app/buscador-remates/buscador-remates.component.ts
+++ SubastasWeb/frontend/src/app/buscador-remates/buscador-remates.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { FormsModule } from '@angular/forms';
@@ -6,7 +7,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-buscador-remates',
   standalone: true,
-  imports: [ButtonModule, DropdownModule, FormsModule],
+  imports: [CommonModule, ButtonModule, DropdownModule, FormsModule],
   templateUrl: './buscador-remates.component.html',
   styleUrl: './buscador-remates.component.scss'
 })
@@ -18,5 +19,22 @@ export class BuscadorRematesComponent {
     { name: 'Concepción', code: 'CCP' }
   ];
 
+  subastas = [
+    { name: 'Subasta 1', informacions: 'S1', imagen: '' },
+    { name: 'Subasta 2', informacion: 'S2', imagen: '' },
+    { name: 'Subasta 3', informacion: 'S3', imagen: '' },
+    { name: 'Subasta 4', informacion: 'S4', imagen: '' },
+    { name: 'Subasta 5', informacion: 'S5', imagen: '' },
+    { name: 'Subasta 6', informacion: 'S6', imagen: '' },
+  ]
+  
   selectedCity: any = null;
+
+  get subastasPorGrupo(): any[][] {
+    const grupos = [];
+    for (let i = 0; i < this.subastas.length; i += 3) {
+      grupos.push(this.subastas.slice(i, i + 3));
+    }
+    return grupos;
+  }
 }
