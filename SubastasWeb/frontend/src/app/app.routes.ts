cba11536d56b93232790f4257ee9c46e1516d601
renzoPeraza <renzo.peraza@estudiantes.utec.edu.uy>
2025-05-27 18:29:53 -0300

    Paginas Perfil y Subir Producto
    Faltas muchos arreglos visuales, en greandes razgos estan hechas, son muuuy feas

--- SubastasWeb/frontend/src/app/app.routes.ts
+++ SubastasWeb/frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { StreamComponent } from './stream/stream.component';
 import { BuscadorRematesComponent } from './buscador-remates/buscador-remates.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { SubirProductoComponent } from './subir-producto/subir-producto.component';
 
 export const routes: Routes = [
     { path: 'stream', loadComponent: () => StreamComponent },
     { path: 'buscadorRemates', loadComponent: () => BuscadorRematesComponent },
+    { path: 'perfil', loadComponent: () => PerfilComponent },
+    { path: 'subirProducto', loadComponent: () => SubirProductoComponent }
 ];
