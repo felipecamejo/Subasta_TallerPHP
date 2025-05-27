00bfc113fed62415551b6194b9092fa00f75065c
Alcedepradera <pablo.deleon@estudiantes.utec.edu.uy>
2025-05-26 21:27:49 -0300

    los controlles 0km
    recien creados por el artesano.

---
+++ SubastasWeb/app/Http/Controllers/PujaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Puja;
+use Illuminate\Http\Request;
+
+class PujaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Puja $puja)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Puja $puja)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Puja $puja)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Puja $puja)
+    {
+        //
+    }
+}
