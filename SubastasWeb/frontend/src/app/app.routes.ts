e2c0f07103a1855226e51a2d6610569e0500f4c3
Felipe <23felipecamejo@gmail.com>
2025-05-27 19:32:43 -0300

    listo casi

--- SubastasWeb/frontend/src/app/app.routes.ts
+++ SubastasWeb/frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { StreamComponent } from './stream/stream.component';
 import { BuscadorRematesComponent } from './buscador-remates/buscador-remates.component';
+import { CasaRemateComponent } from './casa-remate/casa-remate.component';
 
 export const routes: Routes = [
     { path: 'stream', loadComponent: () => StreamComponent },
     { path: 'buscadorRemates', loadComponent: () => BuscadorRematesComponent },
+    { path: 'casaRemate', loadComponent: () => CasaRemateComponent},
 ];
