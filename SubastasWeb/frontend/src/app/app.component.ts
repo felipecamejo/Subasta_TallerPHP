3719c80315ddf7c1f5afc308d63245c8ef1ca7f4
Alcedepradera <pablo.deleon@estudiantes.utec.edu.uy>
2025-05-27 21:06:34 -0300

    cp

--- SubastasWeb/frontend/src/app/app.component.ts
+++ SubastasWeb/frontend/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] // <-- "styleUrls" en plural y con array
 })
 export class AppComponent {
   title = 'frontend';
--- SubastasWeb/frontend/src/app/app.routes.ts
+++ SubastasWeb/frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { StreamComponent } from './stream/stream.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'stream', loadComponent: () => StreamComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
+
