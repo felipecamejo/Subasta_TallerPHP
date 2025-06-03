1b48ecd98509e681be29fc2040369c4946c60bcc
Matias <maatias.echeverria@gmail.com>
2025-06-03 18:42:35 -0300

    casa remates html

--- SubastasWeb/frontend/src/app/app.routes.ts
+++ SubastasWeb/frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { StreamComponent } from './stream/stream.component';
 import { LoginComponent } from './login/login.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
+import { CasaRemateComponent } from './casa-remate/casa-remate.component';
 
 export const routes: Routes = [
     { path: 'stream/:id', loadComponent: () => StreamComponent },
+    { path: 'estadisticas', loadComponent: () => EstadisticasComponent },
+    { path: 'casa-remates', loadComponent: () => CasaRemateComponent },
 ];
 
