c539f930074e373917057b7bc4a82807e4d6780d
Alcedepradera <pablo.deleon@estudiantes.utec.edu.uy>
2025-06-04 10:04:45 -0300

    Revert "botones registro login"
    This reverts commit 7bcc524771242e27f52a6a032b820757f9db2143.

--- SubastasWeb/frontend/src/app/app.routes.ts
+++ SubastasWeb/frontend/src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
+import { StreamComponent } from './stream/stream.component';
+import { BuscadorRematesComponent } from './buscador-remates/buscador-remates.component';
+import { PerfilComponent } from './perfil/perfil.component';
 import { LoginComponent } from './login/login.component';
-import { AuthSelectorComponent } from './auth-selector/auth-selector.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
+import { CasaRemateComponent } from './casa-remate/casa-remate.component';
+import { RegisterComponent } from './register/register.component';
+
 
 export const routes: Routes = [
-  { path: '', component: AuthSelectorComponent },  // Pantalla inicial con botones Login y Registro
-  { path: 'register', component: RegisterComponent }, 
-  { path: 'login', component: LoginComponent },
-  // Podés agregar aquí rutas para otras páginas, por ejemplo home, dashboard, etc.
+    { path: 'stream/:id', component: StreamComponent },
+    { path: 'buscadorRemates', component: BuscadorRematesComponent},
+    { path: 'perfil/:id', component: PerfilComponent },
+    { path: 'estadisticas', component: EstadisticasComponent },
+    { path: 'casa-remates', component: CasaRemateComponent },
+    { path: 'registro', component: RegisterComponent },
+    { path: '', redirectTo: 'registro', pathMatch: 'full' }, 
 ];
+
