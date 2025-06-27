018f7346e144ca0ccce35356fe7901bf7ea7dbfc
Alcedepradera <pablo.deleon@estudiantes.utec.edu.uy>
2025-06-27 11:45:47 -0300

    lo ultimo

--- SubastasWeb/frontend/src/environments/environment.ngrok.ts
+++ SubastasWeb/frontend/src/environments/environment.ngrok.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://09e7-167-56-104-67.ngrok-free.app' // Reemplazá con el correcto
+  apiUrl: 'https://remates.ngrok.app' // Reemplazá con el correcto
 };
