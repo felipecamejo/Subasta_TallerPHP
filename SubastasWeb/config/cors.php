1a7e3abccab54b9731f7dba543d52214787c513e
Felipe <23felipecamejo@gmail.com>
2025-06-28 16:48:50 -0300

    arregle error de cors

--- SubastasWeb/config/cors.php
+++ SubastasWeb/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'], // Permite rutas de la API y la ruta de CSRF de Sanctum
     'allowed_methods' => ['*'], // Permite todos los métodos HTTP
-    'allowed_origins' => ['http://localhost:4200', 'https://53fd59719a16.ngrok.app'],
+    'allowed_origins' => ['*'], // Permite todos los orígenes (solo para desarrollo)
     'allowed_headers' => ['*'], // Permite todos los encabezados
     'exposed_headers' => [],
     'max_age' => 0,
