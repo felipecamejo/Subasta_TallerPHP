26455af8484334327c421d6a81453206afbb6389
Felipe <23felipecamejo@gmail.com>
2025-06-29 22:40:01 -0300

    borre una relacion inesecaria

--- SubastasWeb/app/Models/CasaRemate.php
+++ SubastasWeb/app/Models/CasaRemate.php
@@ -28,10 +28,6 @@
             return $this->belongsTo(Usuario::class, 'usuario_id');
         }
 
-        public function rematador(){
-            return $this->hasOne(Rematador::class, 'usuario_id');
-        }
-
         public function valoracion(){
             return $this->hasOne(Valoracion::class, 'valorable_id', 'usuario_id');
         }
